4517fa4405a4eb4db9a512ff73193579af1b38da
Ali Bagdatli <144359335+AliBag-de@users.noreply.github.com>
2025-07-27 01:06:21 +0200

    Update Body.tsx

--- components/Body.tsx
+++ components/Body.tsx
@@ -252,7 +252,7 @@ const Body = ({
                       variant="outline"
                       onClick={() => {
                         navigator.clipboard.writeText(
-                          `https://qrgpt.io/start/${id || ''}`,
+                          `https://canyoube.fun/start/${id || ''}`,
                         );
                         toast.success('Link copied to clipboard');
                       }}
